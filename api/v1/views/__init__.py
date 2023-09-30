0952983f21539328eb2fa8ff07450868b5140bd6
Yousef Saeed <yousafesaeed@gmail.com>
2023-09-30 08:57:04 +0300

    Changes & Fixes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""API Blueprint for version 1 of the App.
-
-This module initializes the Flask blueprint for version 1 of the API.
-It sets the URL prefix for all routes under this version to '/api/v1'.
-The blueprint is used to organize and register the various view modules
-that handle specific endpoints of the API.
-"""
+"""API Blueprint for version 1 of the App."""
 
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from api.v1.views.users import *
+from api.v1.views.cities import *
 from api.v1.views.places import *
-from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""Routes Handling for the App.
-
-This module contains route handlers for the Flask app.
-It defines the various routes and their corresponding functions
-to handle incoming HTTP requests.
-Each route is responsible for a specific endpoint or functionality of the app.
-"""
+"""Routes Handling for the App."""
 
 from api.v1.views import app_views
-from flask import Response, jsonify
+from flask import Flask, Blueprint, jsonify
 from models import storage
-from models.engine.db_storage import classes
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def check_status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats")
-def num_objs():
+@app_views.route("/stats", strict_slashes=False)
+def objects_count():
     """Retrieves the number of each objects by type."""
     objects = {
-        "amenities": storage.count(classes["Amenity"]),
-        "cities": storage.count(classes["City"]),
-        "places": storage.count(classes["Place"]),
-        "reviews": storage.count(classes["Review"]),
-        "states": storage.count(classes["State"]),
-        "users": storage.count(classes["User"]),
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
     }
-    return jsonify(objects)
+    objects_dict = {}
+    for key, value in objects.items():
+        objects_dict[key] = storage.count(value)
+    return jsonify(objects_dict)
+
+
+if __name__ == "__main__":
+    pass
