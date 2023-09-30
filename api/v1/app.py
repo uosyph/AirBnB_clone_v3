c89d418086046471a6d3fc5d596ef9ec0e2908e4
Yousef Saeed <yousafesaeed@gmail.com>
2023-10-01 00:32:12 +0300

    Reverse some recent changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-"""Flask web server to handle API requests."""
+"""Flask web server to handle API requests.
 
-import os
-from models import storage
+This module sets up a Flask web server that handles API requests.
+It registers the necessary routes and configurations
+to respond to HTTP requests.
+The server listens on the specified host and port,
+which can be customized through environment variables.
+"""
+
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Blueprint, Flask, jsonify
 from flask_cors import CORS
+from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.url_map.strict_slashes = False
+
+CORS(app, origins="0.0.0.0")
+
+api_host = getenv("HBNB_API_HOST", "0.0.0.0")
+api_port = getenv("HBNB_API_PORT", 5000)
 
 
 @app.teardown_appcontext
@@ -22,9 +34,8 @@ def teardown(self):
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 errors and returns a JSON-formatted 404 response."""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(os.getenv("HBNB_API_PORT", "5000")))
+    app.run(host=api_host, port=int(api_port), threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""API Blueprint for version 1 of the App."""
+"""API Blueprint for version 1 of the App.
+
+This module initializes the Flask blueprint for version 1 of the API.
+It sets the URL prefix for all routes under this version to '/api/v1'.
+The blueprint is used to organize and register the various view modules
+that handle specific endpoints of the API.
+"""
 
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.places import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places_reviews import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
